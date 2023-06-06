47863aa8d8bac7439c94e66306483a37bdfa0ca7
Akshay Sharma <54117867+Akshay2131@users.noreply.github.com>
2023-06-06 23:51:28 +0530

    Update Application.java

--- src/main/java/com/akshay/blog/Application.java
+++ src/main/java/com/akshay/blog/Application.java
@@ -2,7 +2,7 @@ package com.akshay.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Application class to run spring application
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
