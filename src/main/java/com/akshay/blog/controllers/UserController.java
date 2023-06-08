39e2716c39ada2ddbb480d76d53102393683828f
Akshay <aksharma213123@gmail.com>
2023-06-09 00:09:16 +0530

    TestClassAdded

--- src/main/java/com/akshay/blog/controllers/UserController.java
+++ src/main/java/com/akshay/blog/controllers/UserController.java
@@ -5,10 +5,7 @@ import com.akshay.blog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,7 +13,7 @@ public class UserController {
     @Autowired
     private UserService userService;
     // POST - Create User
-    @GetMapping("/")
+    @PostMapping("/")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
         UserDto createUserDto = this.userService.createUser(userDto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
