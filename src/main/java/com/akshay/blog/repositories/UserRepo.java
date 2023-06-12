212adf6d7ea431a6f554ea91cdbf2896c1aed059
Akshay <aksharma213123@gmail.com>
2023-06-12 23:36:22 +0530

    User updated

--- src/main/java/com/akshay/blog/repositories/UserRepo.java
+++ src/main/java/com/akshay/blog/repositories/UserRepo.java
@@ -2,6 +2,8 @@ package com.akshay.blog.repositories;
 
 import com.akshay.blog.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
 }
